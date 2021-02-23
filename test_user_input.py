e2ab020672894c0014e9ee4eed9a5e27443f9dac
Omer Koskas <omerk96@gmail.com>
2021-02-23 19:07:24 +0200

    Added handling of user input
    The code now supports handling the user's input. Also moved most of the logic from main to a different module, so it will be easier to test.

---
+++ test_user_input.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass
+import user_input
+
+
+@dataclass
+class ProgramArgs:
+    n: int
+    k: int
+    random: bool
+
+
+class TestCheckUserInput:
+    def test_random_args(self):
+        args = ProgramArgs(0, 0, True)
+        assert user_input.check_user_input(args)
+
+    def test_zero_n(self):
+        args = ProgramArgs(0, 1, False)
+        assert not user_input.check_user_input(args)
+
+    def test_negative_n(self):
+        args = ProgramArgs(-1, 1, False)
+        assert not user_input.check_user_input(args)
+
+    def test_zero_k(self):
+        args = ProgramArgs(1, 0, False)
+        assert not user_input.check_user_input(args)
+
+    def test_negative_k(self):
+        args = ProgramArgs(1, -1, False)
+        assert not user_input.check_user_input(args)
+
+    def test_equal_n_k(self):
+        args = ProgramArgs(1, 1, False)
+        assert not user_input.check_user_input(args)
+
+    def test_smaller_n_k(self):
+        args = ProgramArgs(1, 2, False)
+        assert not user_input.check_user_input(args)
+
+    def test_bigger_n_k(self):
+        args = ProgramArgs(2, 1, False)
+        assert user_input.check_user_input(args)
+
+
+class TestGeneratePoints:
+    def test_random_points(self):
+        guaranteed_random = False
+        args = ProgramArgs(50, 10, True)
+
+        # Probability test of randomness
+        for _ in range(10):
+            params, points, centers = user_input.generate_points(args)
+            if params.n != args.n or params.k != args.k:
+                guaranteed_random = True
+            assert len(points) == params.n
+            assert len(centers) == params.n
+            assert params.random
+            assert params.dim in [2, 3]
+
+        assert guaranteed_random
+
+    def test_non_random_points(self):
+        args = ProgramArgs(50, 10, False)
+        params, points, centers = user_input.generate_points(args)
+
+        assert params.n == args.n
+        assert params.k == args.k
+        assert not params.random
+        assert params.dim in [2, 3]
+        assert len(points) == args.n
+        assert len(centers) == args.n
